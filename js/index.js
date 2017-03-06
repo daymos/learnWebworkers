e19d4a7eb4e41dd7328cac73b8096b7086d26f2c
MattiaS <mattia.spinelli@moogsoft.com>
2017-03-06 23:21:48 +0000

    first webworker

--- js/index.js
+++ js/index.js
@@ -4,4 +4,5 @@ worker.addEventListener('message', function(e) {
     console.log('Worker said: ', e.data);
 }, false);
 
+// the mehtod postMessage is used to comunicate with the worker. Can take json.
 worker.postMessage('Hello World'); // Send data to our worker.
--- js/twitterWorker.js
+++ js/twitterWorker.js
@@ -1,1 +1,5 @@
-console.log('hello')
+var a = require('myscript.js');
+// this receive the content of postMessage in the parent script
+self.addEventListener('message', function(e) {
+  self.postMessage(e.data);// this line return something to the parent
+}, false);
